7a0e157d0cf78111dd6d13172c3a7845bd104838
Scott Cummings <5125421+BeldrothTheGold@users.noreply.github.com>
2022-12-18 19:33:44 -0700

    Add keyboard input debug output

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use crate::{
-    rustominos::RotationDirection, rustris_board::RustrisBoard,
-    rustris_controller::RustrisController, rustris_view::RustrisView,
+    rustominos::RotationDirection,
+    rustris_board::RustrisBoard,
+    rustris_controller::{RustrisController, RustrisOptions},
+    rustris_view::RustrisView,
 };
 use piston_window::{types::Color, *};
 use rustominos::{Rustomino, RustominoType};
@@ -12,12 +14,13 @@ mod rustris_controller;
 mod rustris_view;
 
 const BACKGROUND_COLOR: Color = [0.0, 0.0, 0.0, 1.0];
+const WINDOW_DIMENSIONS: [u32; 2] = [1024, 768];
 
 fn main() {
     env_logger::init_from_env("RUSTRIS_LOG_LEVEL");
     log::info!("Startup: Initializing Piston Window");
     let mut window: piston_window::PistonWindow =
-        piston_window::WindowSettings::new("Rustris", [1280, 720])
+        piston_window::WindowSettings::new("Rustris", WINDOW_DIMENSIONS)
             .resizable(false)
             .exit_on_esc(true)
             .vsync(true)
@@ -31,6 +34,9 @@ fn main() {
     while let Some(event) = window.next() {
         if let Some(Button::Keyboard(key)) = event.press_args() {
             rustris_controller.key_pressed(key);
+        }
+        if let Some(Button::Keyboard(key)) = event.release_args() {
+            rustris_controller.key_released(key);
         }
         window.draw_2d(&event, |c, g, _| {
             clear(BACKGROUND_COLOR, g);
