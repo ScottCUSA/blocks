2e1dfe8a0ccf611baba9033675bd44129c52ca0f
Scott Cummings <5125421+BeldrothTheGold@users.noreply.github.com>
2023-01-05 15:56:10 -0700

    fix drawing and input - UI text

--- src/game.rs
+++ src/game.rs
@@ -39,7 +39,7 @@ const SOFT_DROP_KEYS: [Option<KeyCode>; 2] = [Some(KeyCode::Down), None];
 const HARD_DROP_KEYS: [Option<KeyCode>; 2] = [Some(KeyCode::Space), None];
 const HOLD_KEYS: [Option<KeyCode>; 2] = [Some(KeyCode::LeftShift), Some(KeyCode::C)];
 
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 enum KeyState {
     Up,
     Down(f64),
@@ -52,7 +52,7 @@ impl Default for KeyState {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumIter)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, EnumIter)]
 enum Inputs {
     Left,
     Right,
@@ -84,17 +84,34 @@ impl Inputs {
             _ => None,
         }
     }
+    fn default_keys(&self) -> [Option<KeyCode>; 2] {
+        match self {
+            Inputs::Left => LEFT_KEYS,
+            Inputs::Right => RIGHT_KEYS,
+            Inputs::RotateCW => ROTATE_CW_KEYS,
+            Inputs::RotateCCW => ROTATE_CCW_KEYS,
+            Inputs::SoftDrop => SOFT_DROP_KEYS,
+            Inputs::HardDrop => HARD_DROP_KEYS,
+            Inputs::Hold => HOLD_KEYS,
+        }
+    }
 }
 
-pub struct InputController {
-    input_key_map: HashMap<KeyCode, Inputs>,
+pub struct GameInputs {
+    input_map: HashMap<Inputs, [Option<KeyCode>; 2]>,
+    key_map: HashMap<KeyCode, Inputs>,
     input_states: HashMap<Inputs, KeyState>,
 }
 
-impl Default for InputController {
+impl Default for GameInputs {
     fn default() -> Self {
         Self {
-            input_key_map: {
+            input_map: {
+                Inputs::iter()
+                    .map(|i| (i.clone(), i.default_keys()))
+                    .collect()
+            },
+            key_map: {
                 LEFT_KEYS
                     .iter()
                     .flatten()
@@ -156,8 +173,7 @@ pub struct RustrisGame {
     pub game_state: GameState,
     pub score: usize,
     pub game_level: usize,
-    hold_set: bool,
-    input_controller: InputController,
+    inputs: GameInputs,
     rustomino_bag: Vec<RustominoType>,
     rng: rand_xoshiro::Xoshiro256PlusPlus,
     gravity_time_accum: f64,
@@ -165,10 +181,11 @@ pub struct RustrisGame {
     completed_lines: usize,
     last_update: f64,
     view_settings: ViewSettings,
+    hold_set: bool,
 }
 
 impl RustrisGame {
-    pub fn new(board: RustrisBoard) -> Self {
+    pub fn new(board: RustrisBoard, view_settings: ViewSettings) -> Self {
         RustrisGame {
             board,
             next_rustomino: None,
@@ -178,7 +195,7 @@ impl RustrisGame {
             game_level: 1,
             hold_set: false,
             // rng: rand_xoshiro::Xoshiro256PlusPlus::seed_from_u64(DEBUG_RNG_SEED), // FOR DEBUGING
-            input_controller: InputController::default(),
+            inputs: GameInputs::default(),
             // input_controller: InputController::from (),
             rustomino_bag: Vec::new(),
             rng: rand_xoshiro::Xoshiro256PlusPlus::from_entropy(),
@@ -186,7 +203,7 @@ impl RustrisGame {
             gravity_delay: gravity_delay(1), // self.options.gravity_delay(game_level)
             completed_lines: 0,
             last_update: get_time(),
-            view_settings: ViewSettings::new([1024., 768.]),
+            view_settings,
         }
         .init()
     }
@@ -198,67 +215,12 @@ impl RustrisGame {
         self
     }
 
-    pub async fn run(self) {
-        scene::add_node(self);
-
-        loop {
-            next_frame().await
-        }
-    }
-
-    pub fn key_pressed(&mut self, key: KeyCode) {
-        // allow the user to rotate the rustomino with the left and right arrows
-        // allow the user to fast drop the rustomino with the down arrow key
-        log::debug!("key pressed: {:?}", key);
-
-        match self.game_state {
-            GameState::Menu => todo!(),
-            GameState::Playing => {
-                if let Some(input) = self.input_controller.input_key_map.get(&key) {
-                    self.input_controller
-                        .input_states
-                        .entry(*input)
-                        .and_modify(|e| *e = KeyState::Down(0.0));
-                    match input {
-                        Inputs::Left => self.translate(TranslationDirection::Left),
-                        Inputs::Right => self.translate(TranslationDirection::Right),
-                        Inputs::RotateCW => self.rotate(RotationDirection::Cw),
-                        Inputs::RotateCCW => self.rotate(RotationDirection::Ccw),
-                        Inputs::SoftDrop => self.soft_drop(),
-                        Inputs::HardDrop => self.hard_drop(),
-                        Inputs::Hold => self.hold(),
-                    }
-                }
-            }
-            GameState::GameOver => todo!(),
-        }
-    }
-
-    pub fn key_released(&mut self, key: KeyCode) {
-        // allow the user to rotate the rustomino with the left and right arrows
-        // allow the user to fast drop the rustomino with the down arrow key
-        log::debug!("key released: {:?}", key);
-        match self.game_state {
-            GameState::Playing => {
-                if let Some(input) = self.input_controller.input_key_map.get(&key) {
-                    self.input_controller
-                        .input_states
-                        .entry(*input)
-                        .and_modify(|e| *e = KeyState::Up)
-                        .or_insert(KeyState::default());
-                }
-            }
-            GameState::Menu => todo!(),
-            GameState::GameOver => todo!(),
-        }
-    }
-
     fn handle_inputs(&mut self, delta_time: f64) {
         // check each input
         for input in Inputs::iter() {
-            self.input_controller
+            self.inputs
                 .input_states
-                .entry(input)
+                .entry(input.clone())
                 .and_modify(|e| match e {
                     KeyState::Down(down_time) => {
                         if let Some(action_delay) = input.action_delay_for_input() {
@@ -273,7 +235,7 @@ impl RustrisGame {
                     }
                     _ => (),
                 });
-            if let Some(state) = self.input_controller.input_states.get_mut(&input) {
+            if let Some(state) = self.inputs.input_states.get_mut(&input) {
                 match state {
                     KeyState::Held(held_time) => {
                         if let Some(action_repeat_delay) = input.action_repeat_delay_for_input() {
@@ -453,122 +415,181 @@ impl RustrisGame {
             self.score
         )
     }
-}
 
-impl scene::Node for RustrisGame {
-    fn draw(node: scene::RefMut<Self>) {
-        clear_background(view::BACKGROUND_COLOR);
-
-        draw_rectangle(
-            node.view_settings.staging_rect.x,
-            node.view_settings.staging_rect.y,
-            node.view_settings.staging_rect.w,
-            node.view_settings.staging_rect.h,
-            view::STAGING_BACKGROUND_COLOR,
-        );
-
-        draw_rectangle(
-            node.view_settings.board_rect.x,
-            node.view_settings.board_rect.y,
-            node.view_settings.board_rect.w,
-            node.view_settings.board_rect.h,
-            view::BOARD_BACKGROUND_COLOR,
-        );
-
-        draw_rectangle(
-            node.view_settings.preview_rect.x,
-            node.view_settings.preview_rect.y,
-            node.view_settings.preview_rect.w,
-            node.view_settings.preview_rect.h,
-            view::PREVIEW_BACKGROUND_COLOR,
-        );
-
-        draw_rectangle(
-            node.view_settings.hold_rect.x,
-            node.view_settings.hold_rect.y,
-            node.view_settings.hold_rect.w,
-            node.view_settings.hold_rect.h,
-            view::HOLD_BACKGROUND_COLOR,
-        );
-
-        for (y, slots_x) in node.board.slots.iter().enumerate() {
-            for (x, slot) in slots_x.iter().enumerate() {
-                match slot {
-                    SlotState::Locked(rtype) => {
-                        // draw the block
-                        let rect = board_block_rect([x as i32, y as i32], &node.view_settings);
-                        draw_rectangle(rect.x, rect.y, rect.w, rect.h, rtype.color());
-                    }
-                    _ => {}
-                }
-            }
-        }
-
-        for rustomino in node.board.current_rustomino.iter() {
-            for slot in rustomino.board_slots() {
-                // display the preview
-                // draw the block
-                let rect = board_block_rect([slot[0], slot[1]], &node.view_settings);
-                draw_rectangle(
-                    rect.x,
-                    rect.y,
-                    rect.w,
-                    rect.h,
-                    rustomino.rustomino_type.color(),
-                );
-            }
-        }
-
-        for ghost in node.board.ghost_rustomino.iter() {
-            for block in ghost.board_slots() {
-                // draw the block
-                let rect = board_block_rect([block[0], block[1]], &node.view_settings);
-                draw_rectangle(rect.x, rect.y, rect.w, rect.h, view::GHOST_COLOR);
-            }
-        }
-    }
-
-    fn update(mut node: scene::RefMut<Self>)
-    where
-        Self: Sized,
-    {
-        let now = get_time();
-        let delta_time = now - node.last_update;
-
-        match node.game_state {
+    pub fn draw(&self) {
+        match self.game_state {
             GameState::Menu => todo!(),
             GameState::Playing => {
-                // check board ready for the next rustomino
-                if node.board.ready_for_next() {
-                    // TODO: move this whole block to a fn
-                    // take the next rustomino
-                    // unwrap should be safe here
-                    let current_rustomino = node.next_rustomino.take().unwrap();
-                    // we used next_rustomino so we need to replace it
-                    node.set_next_rustomino();
-                    // add the next rustomino to the board
-                    // game over if it can't be placed without a collision
-                    if !node.board.add_new_rustomino(current_rustomino) {
-                        node.game_over();
+                draw_rectangle(
+                    self.view_settings.staging_rect.x,
+                    self.view_settings.staging_rect.y,
+                    self.view_settings.staging_rect.w,
+                    self.view_settings.staging_rect.h,
+                    view::STAGING_BACKGROUND_COLOR,
+                );
+
+                draw_rectangle(
+                    self.view_settings.board_rect.x,
+                    self.view_settings.board_rect.y,
+                    self.view_settings.board_rect.w,
+                    self.view_settings.board_rect.h,
+                    view::BOARD_BACKGROUND_COLOR,
+                );
+
+                draw_rectangle(
+                    self.view_settings.preview_rect.x,
+                    self.view_settings.preview_rect.y,
+                    self.view_settings.preview_rect.w,
+                    self.view_settings.preview_rect.h,
+                    view::PREVIEW_BACKGROUND_COLOR,
+                );
+
+                draw_rectangle(
+                    self.view_settings.hold_rect.x,
+                    self.view_settings.hold_rect.y,
+                    self.view_settings.hold_rect.w,
+                    self.view_settings.hold_rect.h,
+                    view::HOLD_BACKGROUND_COLOR,
+                );
+
+                for (y, slots_x) in self.board.slots.iter().enumerate() {
+                    for (x, slot) in slots_x.iter().enumerate() {
+                        match slot {
+                            SlotState::Locked(rtype) => {
+                                // draw the block
+                                let rect =
+                                    board_block_rect([x as i32, y as i32], &self.view_settings);
+                                draw_rectangle(rect.x, rect.y, rect.w, rect.h, rtype.color());
+                            }
+                            _ => {}
+                        }
                     }
                 }
-                node.handle_inputs(delta_time);
-                // Apply "gravity" to move the current rustomino down the board
-                // or if it can't move lock it
-                node.gravity_time_accum += delta_time;
-                if node.gravity_time_accum >= node.gravity_delay {
-                    node.gravity_time_accum = 0.0;
-                    node.gravity_tick();
+
+                if let Some(next) = &self.next_rustomino {
+                    for slot in next.blocks {
+                        // display the preview
+                        // draw the block
+                        let rect = next_block_rect([slot[0], slot[1]], &self.view_settings);
+                        draw_rectangle(rect.x, rect.y, rect.w, rect.h, next.rustomino_type.color());
+                    }
                 }
 
-                // increase the game level every LINES_PER_LEVEL
-                if node.completed_lines > node.game_level * LINES_PER_LEVEL {
-                    node.increase_game_level();
+                if let Some(held) = &self.hold_rustomino {
+                    for slot in held.blocks {
+                        // display the preview
+                        // draw the block
+                        let rect = hold_block_rect([slot[0], slot[1]], &self.view_settings);
+                        draw_rectangle(rect.x, rect.y, rect.w, rect.h, held.rustomino_type.color());
+                    }
+                }
+
+                if let Some(rustomino) = &self.board.current_rustomino {
+                    for slot in rustomino.board_slots() {
+                        // display the preview
+                        // draw the block
+                        let rect = board_block_rect([slot[0], slot[1]], &self.view_settings);
+                        draw_rectangle(
+                            rect.x,
+                            rect.y,
+                            rect.w,
+                            rect.h,
+                            rustomino.rustomino_type.color(),
+                        );
+                    }
+                }
+
+                if let Some(ghost) = &self.board.ghost_rustomino {
+                    for block in ghost.board_slots() {
+                        // draw the block
+                        let rect = board_block_rect([block[0], block[1]], &self.view_settings);
+                        draw_rectangle_lines(
+                            rect.x,
+                            rect.y,
+                            rect.w,
+                            rect.h,
+                            2.0,
+                            view::GHOST_COLOR,
+                        );
+                    }
                 }
             }
             GameState::GameOver => todo!(),
         }
-        node.last_update = now;
+    }
+
+    pub fn update(&mut self) {
+        let now = get_time();
+        let delta_time = now - self.last_update;
+
+        match self.game_state {
+            GameState::Menu => todo!(),
+            GameState::Playing => {
+                // check board ready for the next rustomino
+                if self.board.ready_for_next() {
+                    // TODO: move this whole block to a fn
+                    // take the next rustomino
+                    // unwrap should be safe here
+                    let current_rustomino = self.next_rustomino.take().unwrap();
+                    // we used next_rustomino so we need to replace it
+                    self.set_next_rustomino();
+                    // add the next rustomino to the board
+                    // game over if it can't be placed without a collision
+                    if !self.board.add_new_rustomino(current_rustomino) {
+                        self.game_over();
+                    }
+                }
+                self.handle_keys();
+                self.handle_inputs(delta_time);
+                // Apply "gravity" to move the current rustomino down the board
+                // or if it can't move lock it
+                self.gravity_time_accum += delta_time;
+                if self.gravity_time_accum >= self.gravity_delay {
+                    self.gravity_time_accum = 0.0;
+                    self.gravity_tick();
+                }
+
+                // increase the game level every LINES_PER_LEVEL
+                if self.completed_lines > self.game_level * LINES_PER_LEVEL {
+                    self.increase_game_level();
+                }
+            }
+            GameState::GameOver => todo!(),
+        }
+        self.last_update = now;
+    }
+
+    fn handle_keys(&mut self) {
+        match self.game_state {
+            GameState::Menu => todo!(),
+            GameState::Playing => {
+                for (input, keys) in &self.inputs.input_map.clone() {
+                    for key in keys.iter().flatten() {
+                        if is_key_pressed(*key) {
+                            self.inputs
+                                .input_states
+                                .entry(input.clone())
+                                .and_modify(|e| *e = KeyState::Down(0.0));
+                            match input {
+                                Inputs::Left => self.translate(TranslationDirection::Left),
+                                Inputs::Right => self.translate(TranslationDirection::Right),
+                                Inputs::RotateCW => self.rotate(RotationDirection::Cw),
+                                Inputs::RotateCCW => self.rotate(RotationDirection::Ccw),
+                                Inputs::SoftDrop => self.soft_drop(),
+                                Inputs::HardDrop => self.hard_drop(),
+                                Inputs::Hold => self.hold(),
+                            }
+                        } else if is_key_released(*key) {
+                            self.inputs
+                                .input_states
+                                .entry(input.clone())
+                                .and_modify(|e| *e = KeyState::Up);
+                        }
+                    }
+                }
+            }
+            GameState::GameOver => todo!(),
+        }
     }
 }
 
